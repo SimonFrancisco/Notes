29e9de1364ffd868e1c06654d1ea34a0770c7e20
Simon Francisco <121758120+SimonFrancisco@users.noreply.github.com>
2024-10-17 00:09:37 +0700

    Data start

--- app/src/main/java/com/example/notes/domain/entity/Note.kt
+++ app/src/main/java/com/example/notes/domain/entity/Note.kt
@@ -4,8 +4,8 @@ data class Note(
     val id: Int = DEFAULT_ID,
     val topic: String,
     val content: String,
-    val isDraft: Boolean = false,
-    val isFavourite: Boolean = false
+    val isDraft: Boolean,
+    val isFavourite: Boolean
 ) {
     companion object {
         const val DEFAULT_ID = 0
